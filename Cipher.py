2aa51e8225a2b9408754c5d95cbc4082ae8e4afd
wryman <william.ryman@utexas.edu>
2014-04-04 12:19:45 -0500

    both encrypt and decrypt working with sample
    still need to address issues with length of even strings

--- Cipher.py
+++ Cipher.py
@@ -1,6 +1,6 @@
 #  File: Cipher.py
 
-#  Description:  Use a transposition cipher to encode/decode input
+#  Description:  Use a transposition cipher to encode/decode input.txt
 
 #  Student Name:  William Ryman
 
@@ -20,27 +20,30 @@ even_str=''
 odd_str=''
 encrypted_str=''
 decrypted=''
+#Having problems with even lengthed strings
 if option =='E' or option=='e':
 	infile=open('input.txt','r')
 	file_content=infile.read()
 	infile.close()
 	for i in range (0,len(file_content),2):
-		even_str+=file_content[i]
-		odd_str+=file_content[i+1]
-	encrypted_str=odd_str+even_str
+		encrypted_str=odd_str+even_str
 	outfile=open('output.txt','w')
 	outfile.write(encrypted_str)
 	outfile.close()
+	print(encrypted_str)
+
 elif option=='D' or option=='d':
 	infile=open('input.txt','r')
 	file_content=infile.read()
 	infile.close()
-	for i in range(0,len(file_content),2):
-		decrypted+=file_content[len(even)]
-	print (decrypted)
+	for i in range(0,len(file_content)//2,1):
+			even_str+=file_content[len(file_content)//2+i]
+			odd_str+=file_content[i]
+			decrypted+=even_str[i]+odd_str[i]
+	print(decrypted)
+
 else:
 	print('Wrong input. Bye.')
 outfile=open('output.txt','r')
 outcontent=outfile.read()
-outfile.close
-print(outcontent)
+outfile.close()
